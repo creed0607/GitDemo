d4e14004e481866d150401a7544eacad6bc18399
creed0607 <agaur968@gmail.com>
2022-03-24 16:15:21 +0530

    Updated folder structure

---
+++ Regression/FNS/src/Video.java
@@ -0,0 +1,37 @@
+public class Video {
+	
+	String videoName;
+	boolean checkout;
+	int rating;
+	
+	Video(String name){
+		videoName=name;
+	}
+	
+	String getName() {
+System.out.println("This new line of code is added!!!!!!");
+System.out.println("Next day work!!!!!!!!!!");
+		return videoName;
+	}
+	
+	void doCheckout() {
+		checkout = true;
+	}
+	
+	void doReturn() {
+		checkout = false;
+	}
+	
+	void receiveRating(int rating) {
+		this.rating = rating;
+	}
+	
+	int getRating() {
+		return rating;
+	}
+	
+	boolean getCheckout() {
+		return checkout;
+	}
+	
+}
